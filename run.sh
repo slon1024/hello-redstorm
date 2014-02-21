af33efc4639643bfb55274a794e77b8eeee2e7f6
Vladimir <vova@getbase.com>
2014-02-21 06:27:49 +0100

    lock

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ bundle install
 bundle exec redstorm install
 
 #localy
-bundle exec redstorm local lib/hello_redstorm/hello_redstorm_topology.rb
+bundle exec redstorm local lib/hello-redstorm/hello_redstorm_topology.rb
 
 #cluster
 #bundle exec redstorm cluster lib/redstorm-starter/word_count_topology.rb
